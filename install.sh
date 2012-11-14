03d62ed5b9a75e7aff965791794e5cba9a52d2ba
Andreas Boehrnsen <andreas@boehrnsen.de>
2012-11-14 13:09:31 +0800

    improved bash installer

--- install.sh
+++ install.sh
@@ -4,10 +4,38 @@
 SCRIPT_PATH=$(cd "$(dirname "$0")"; pwd)
 cd $SCRIPT_PATH
 
+# configuration
+skip_files=( "install.sh"  "Readme.md" )
+nodot_files=( bin )
+
+
+function in_array() {
+  variable="${1}"
+  shift
+  array=("${@}")
+
+  for (( i = 0 ; i < ${#array[@]} ; i++ ))
+  do
+    if [ "${array[$i]}" == "$variable" ]
+    then
+      return 0 # 0 is true
+    fi
+  done
+
+  return 1
+}
+
+
 for file in *; do
-  if [ "$file" != "install.sh" ]; then
+  if ! in_array "$file" ${skip_files[@]}
+  then
+    prefix="."
+    if in_array "$file" ${nodot_files[@]}; then
+      prefix=""
+    fi
+
     source_file="$SCRIPT_PATH/$file"
-    target_file="$HOME/.$file"
+    target_file="$HOME/$prefix$file"
 
     ln -sniv $source_file $target_file # s-ymbolic n-otfollow i-nteractive v-erbose
   fi
