bba9470a1e9b6114a3cbdb8737c85f03f3754eac
Andreas Boehrnsen <andreas@boehrnsen.de>
2012-11-16 15:01:13 +0800

    added initial git setup

---
+++ setup-git.sh
@@ -0,0 +1,9 @@
+
+echo -n "Enter your name: "
+read git_user_name
+git config --global user.name $git_user_name
+
+echo -n "Enter your email: "
+read git_user_email
+git config --global user.email $git_user_email
+
