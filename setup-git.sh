df87327dbe77bdfd043505b007ac67eddd6357d5
Andreas Boehrnsen <andreas@boehrnsen.de>
2012-11-17 11:02:55 +0800

    fixed git settings

--- setup-git.sh
+++ setup-git.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 
 echo -n "Enter your name: "
 read git_user_name
-git config --global user.name $git_user_name
+git config --global user.name "$git_user_name"
 
 echo -n "Enter your email: "
 read git_user_email
