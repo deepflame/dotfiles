343babac58c6da0b9e2d04e6c32ed6cc0c35d482
Andreas Böhrnsen <andreas@boehrnsen.de>
2013-05-30 11:25:03 +0800

    add more config options and comments

--- setup-git.sh
+++ setup-git.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# set git user
 echo -n "Enter your name: "
 read git_user_name
 git config --global user.name "$git_user_name"
@@ -18,5 +19,28 @@ fi
 # set vim as default editor, also fixes vim issue in osx
 git config --global core.editor /usr/bin/vim
 
-#git config --global credential.helper osxkeychain
-#git config --global credential.helper 'cache --timeout=3600'
+# configure diff / merge tool
+if [ `uname` == "Darwin" ]; then
+  git config --global merge.tool opendiff
+  git config --global diff.tool opendiff
+else
+  git config --global merge.tool meld
+  git config --global diff.tool meld
+fi
+# disable ".orig" backups
+git config --global mergetool.keepBackup false
+
+# automatically track new branches
+#git config --global push.default tracking
+#git config --global branch.autosetupmerge true
+
+# colorize output
+git config --global color.ui auto
+
+# aliases
+#git config --global alias.st status
+#git config --global alias.ci commit
+#git config --global alias.co checkout
+#git config --global alias.br branch
+#git config --global alias.lg "log --format='%Cgreen%h %Cred%cr %Creset%s %Cblue%cn%Cgreen%d'"
+
