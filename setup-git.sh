94729db606c68f19dfa76a491b05198e0f802181
Andreas Böhrnsen <andreas@boehrnsen.de>
2013-05-30 11:24:33 +0800

    activate credential helper

--- setup-git.sh
+++ setup-git.sh
@@ -8,6 +8,13 @@ echo -n "Enter your email: "
 read git_user_email
 git config --global user.email $git_user_email
 
+# activate credential helper
+if [ `uname` == "Darwin" ]; then
+  git config --global credential.helper osxkeychain
+else
+  git config --global credential.helper 'cache --timeout=3600'
+fi
+
 # set vim as default editor, also fixes vim issue in osx
 git config --global core.editor /usr/bin/vim
 
