2bd54d9afe39d749d274207efcc14e79f45f7f40
Andreas Böhrnsen <anb@codewemo.com>
2014-03-11 08:03:25 -0400

    add packages for rbenv

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 apt-get update
+
+# base packages
 apt-get install git vim zsh
+
+# for rbenv
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev
