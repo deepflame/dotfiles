de8308ed51f1e79b4d61cf506561ec94f0d584c2
Andreas Böhrnsen <andreas@frontrunner.io>
2019-04-02 17:08:00 +0000

    add lazygit and use sudo

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-apt-get update
+sudo apt-get update
 
 # base packages
-apt-get install git vim zsh
+sudo apt-get install git vim zsh
 
 # for rbenv
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev
+
+# add lazygit
+sudo add-apt-repository ppa:lazygit-team/release
+sudo apt-get update
+sudo apt-get install lazygit
 
 # map capslock to ctrl key
 setxkbmap -option caps:ctrl_modifier
