a82c72b01096025b89f8fb38e27109684fdc3eb4
Andreas Böhrnsen <andreas@frontrunner.io>
2019-05-06 19:01:01 +0000

    add support for Crystal and Go

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -13,5 +13,15 @@ sudo add-apt-repository ppa:lazygit-team/release
 sudo apt-get update
 sudo apt-get install lazygit
 
+# Crystal programming language
+curl -sL "https://keybase.io/crystal/pgp_keys.asc" | sudo apt-key add -
+echo "deb https://dist.crystal-lang.org/apt crystal main" | sudo tee /etc/apt/sources.list.d/crystal.list
+sudo apt-get update
+sudo apt-get install crystal
+
+# Go lang
+wget https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
+
 # map capslock to ctrl key
 setxkbmap -option caps:ctrl_modifier
+
