8f0cd3aad4a75ee4841796671709079c62a62534
Andreas Böhrnsen <andreas@frontrunner.io>
2020-04-01 11:05:02 +0200

    add crystal to ansible playbook

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-sudo apt-get update
-
-# base packages
-sudo apt-get install git vim zsh
-
 # for rbenv
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev
-
-# add lazygit
-sudo add-apt-repository ppa:lazygit-team/release
-sudo apt-get update
-sudo apt-get install lazygit
-
-# Crystal programming language
-curl -sL "https://keybase.io/crystal/pgp_keys.asc" | sudo apt-key add -
-echo "deb https://dist.crystal-lang.org/apt crystal main" | sudo tee /etc/apt/sources.list.d/crystal.list
-sudo apt-get update
-sudo apt-get install crystal
+#sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev
 
 # Go lang
-wget https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
+#wget https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
 
-# map capslock to ctrl key
-setxkbmap -option caps:ctrl_modifier
+# Ansible
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
 
