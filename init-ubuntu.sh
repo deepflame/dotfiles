889d5c15b98f81fe163b3d13e19b1c0e7e72e16b
Andreas Böhrnsen <andreas@frontrunner.io>
2020-04-04 16:51:15 +0200

    cleanup, set default shell

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# for rbenv
-#sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev
-
-# Go lang
-#wget https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
-
-# Ansible
+# install Ansible
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 
+# run Ansible Playbook
+sudo ansible-playbook ubuntu-playbook.yml
+
+# set Zsh as default shell
+chsh -s $(which zsh)
