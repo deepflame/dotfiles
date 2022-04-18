ad4d889ed1ef31e3d58b63f559408f8a1d0bb6c5
Andreas Boehrnsen <andreas.boehrnsen@frontrunner.io>
2022-04-18 10:06:32 +0200

    add git-plugin, clone Doom-Emacs

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 
 CONFIG=".dotbot.conf.yaml"
 DOTBOT_DIR=".dotbot"
+DOTBOT_PLUGIN_DIR=".dotbot-plugins"
 
 DOTBOT_BIN="bin/dotbot"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -12,4 +13,11 @@ cd "${BASEDIR}"
 git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 git submodule update --init --recursive "${DOTBOT_DIR}"
 
-"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+git -C "${DOTBOT_PLUGIN_DIR}" submodule sync --quiet --recursive
+git submodule update --init --recursive "${DOTBOT_PLUGIN_DIR}"
+
+"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" \
+  -d "${BASEDIR}" \
+  -c "${CONFIG}" \
+  -p "${DOTBOT_PLUGIN_DIR}/dotbot-git/git.py" \
+  "${@}"
